a175a6e3343cc1e310386ec0245d7a927f00b158
Olya Salnikova <rilfayne@yandex.com>
2020-06-13 10:37:59 +0200

    hotfix: исправить замечания первого ревью

--- pages/script.js
+++ pages/script.js
@@ -2,41 +2,39 @@ const editButton = document.querySelector('.profile__edit-button')
 const popup = document.querySelector('.popup')
 const closeButton = popup.querySelector('.popup__close')
 const form = popup.querySelector('.popup__form')
-
-// Открытие и закрытие попапа
-
-const popupToggle = function(event) {
-  popup.classList.toggle('popup_opened')
-}
-
-const closePopup = function(event) {
-  if (event.target !== event.currentTarget) { return }
-  popupToggle()
-}
-
-editButton.addEventListener('click', popupToggle)
-closeButton.addEventListener('click', popupToggle)
-form.addEventListener('submit', popupToggle)
-popup.addEventListener('click', closePopup)
-
-// Изменение данных профиля
-
 let nameInput = document.querySelector('.popup__input_name')
 let descriptionInput = document.querySelector('.popup__input_description')
 let nameProfile = document.querySelector('.profile__name')
 let descriptionProfile = document.querySelector('.profile__description')
 
-const formOpenPopup = function(event) {
-  nameInput.value = nameProfile.textContent;
-  descriptionInput.value = descriptionProfile.textContent;
+// Открытие и закрытие попапа
+
+const popupToggle = function(event) {
+  popup.classList.toggle('popup_opened') // добавляем или убираем класс попапу
+
+  nameInput.value = nameProfile.textContent; // заполняем графу name
+  descriptionInput.value = descriptionProfile.textContent; // заполняем графу description
 }
 
-editButton.addEventListener('click', formOpenPopup)
+// закрытие попапа по клику на полупрозрачный фон
+const closePopup = function(event) {
+  if (event.target !== event.currentTarget) { return } 
+  popupToggle()
+}
+
+editButton.addEventListener('click', popupToggle)
+closeButton.addEventListener('click', popupToggle)
+popup.addEventListener('click', closePopup)
+
+// Изменение данных профиля
 
 const formSubmitHandler = function(event) {
   event.preventDefault();
+  
   nameProfile.textContent = nameInput.value;
   descriptionProfile.textContent = descriptionInput.value;
+
+  popupToggle() // закрываем окно
 }
 
 form.addEventListener('submit', formSubmitHandler)
