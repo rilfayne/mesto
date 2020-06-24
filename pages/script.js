8d4a31fb8c0882da469e463ccfdc59a02735f186
Olya Salnikova <rilfayne@yandex.com>
2020-06-24 12:15:57 +0200

    feat: реализовать заполнение 6 карточек при загрузке

--- pages/script.js
+++ pages/script.js
@@ -2,8 +2,8 @@ const editButton = document.querySelector('.profile__edit-button')
 const popup = document.querySelector('.popup')
 const closeButton = popup.querySelector('.popup__close')
 const form = popup.querySelector('.popup__form')
-let nameInput = document.querySelector('.popup__input_name')
-let descriptionInput = document.querySelector('.popup__input_description')
+let nameInput = popup.querySelector('.popup__input_name')
+let descriptionInput = popup.querySelector('.popup__input_description')
 let nameProfile = document.querySelector('.profile__name')
 let descriptionProfile = document.querySelector('.profile__description')
 
@@ -34,6 +34,64 @@ const formSubmitHandler = function(event) {
 
   popupToggle() // закрываем окно
 }
+
+// Реализуем добавление шести карточек при загрузке страницы
+
+// Массив с данным для шести карточек 
+
+const initialCards = [
+  {
+      name: 'Сибирь',
+      link: 'https://images.unsplash.com/photo-1589659925724-1dbf17849e93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      alt: 'Олени'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://images.unsplash.com/photo-1580474256381-f98bb0532dc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+      alt: 'Сёрфер'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://images.unsplash.com/photo-1552857407-c7e8dec595e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=973&q=80',
+      alt: 'Байкал'
+  },
+  {
+      name: 'Карелия',
+      link: 'https://images.unsplash.com/photo-1559029884-4e34093db5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80',
+      alt: 'Деревня'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://images.unsplash.com/photo-1552321570-b74810c6265d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      alt: 'Ледяная пещера'
+  },
+  {
+      name: 'Тулиновка',
+      link: 'https://images.unsplash.com/photo-1516128935666-9742cf27e24c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80',
+      alt: 'Сосновый лес'
+  }
+];
+
+const placeList = document.querySelector('.places__list')
+
+const addPlace = initialCards.forEach(function (place) {
+    // берем содержимое тега template
+    const placeTemplate = document.querySelector('#place').content
+
+    // клонируем содержимое тега template
+    const placeElement = placeTemplate.cloneNode(true)
+
+    // наполняем сожержимым из массива initialCards
+    placeElement.querySelector('.place__image').src = place.link
+    placeElement.querySelector('.place__image').alt = place.alt
+    placeElement.querySelector('.place__name').textContent = place.name
+
+    // отображаем на странице
+    placeList.append(placeElement)
+});
+
+
+// Слушатели
 
 editButton.addEventListener('click', popupToggle)
 closeButton.addEventListener('click', popupToggle)
