6aff40776d06433082da1325f02070cb261d9f74
Olya Salnikova <rilfayne@yandex.com>
2020-06-28 15:18:47 +0200

    feat: попап для просмотра фотографий

--- pages/script.js
+++ pages/script.js
@@ -1,39 +1,34 @@
 const initialCards = [
   {
       name: 'Сибирь',
-      link: 'https://images.unsplash.com/photo-1589659925724-1dbf17849e93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-      alt: 'Олени'
+      link: 'https://images.unsplash.com/photo-1589659925724-1dbf17849e93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
   },
   {
       name: 'Камчатка',
-      link: 'https://images.unsplash.com/photo-1580474256381-f98bb0532dc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
-      alt: 'Сёрфер'
+      link: 'https://images.unsplash.com/photo-1580474256381-f98bb0532dc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
   },
   {
       name: 'Байкал',
-      link: 'https://images.unsplash.com/photo-1552857407-c7e8dec595e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=973&q=80',
-      alt: 'Байкал'
+      link: 'https://images.unsplash.com/photo-1552857407-c7e8dec595e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=973&q=80'
   },
   {
       name: 'Карелия',
-      link: 'https://images.unsplash.com/photo-1559029884-4e34093db5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80',
-      alt: 'Деревня'
+      link: 'https://images.unsplash.com/photo-1559029884-4e34093db5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80'
   },
   {
       name: 'Байкал',
-      link: 'https://images.unsplash.com/photo-1552321570-b74810c6265d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-      alt: 'Ледяная пещера'
+      link: 'https://images.unsplash.com/photo-1552321570-b74810c6265d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
   },
   {
       name: 'Тулиновка',
-      link: 'https://images.unsplash.com/photo-1516128935666-9742cf27e24c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80',
-      alt: 'Сосновый лес'
+      link: 'https://images.unsplash.com/photo-1516128935666-9742cf27e24c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80'
   }
 ]
 
 const editButton = document.querySelector('.profile__edit-button')
 const popupInfo = document.querySelector('.popup_info')
 const popupPlace = document.querySelector('.popup_place')
+const popupImage = document.querySelector('.popup_image')
 const formInfo = document.querySelector('.popup__form_info')
 const nameInput = document.querySelector('.popup__input_name')
 const descriptionInput = document.querySelector('.popup__input_description')
@@ -42,6 +37,7 @@ const descriptionProfile = document.querySelector('.profile__description')
 const addButton = document.querySelector('.profile__add-button')
 const closeButtonInfo = document.querySelector('.popup__close_info')
 const closeButtonPlace = document.querySelector('.popup__close_place')
+const closeButtonImage = document.querySelector('.popup__close_image')
 const formPlace = document.querySelector('.popup__form_place')
 
 // Открытие попапа Info
@@ -60,6 +56,22 @@ const openPopupPlace = function() {
   popupPlace.classList.add('popup_opened')
 }
 
+// Открытие попапа Image
+
+const openPopupImage = function (evt) {
+  popupImage.classList.add('popup_opened')
+
+  const imageTemplate = document.querySelector('.popup-image-template').content
+  const imageElement = imageTemplate.cloneNode(true)
+  const imageFigure = document.querySelector('.popup__image-fiqure')
+
+  imageElement.querySelector('.popup__image').src = evt.target.src
+  imageElement.querySelector('.popup__image').alt = evt.target.alt
+  imageElement.querySelector('.popup__image-name').textContent = evt.target.alt
+
+  imageFigure.append(imageElement)
+}
+
 // Закрытие попапа Info
 
 const closePopupInfo = function() {
@@ -72,11 +84,20 @@ const closePopupPlace = function() {
   popupPlace.classList.remove('popup_opened')
 }
 
+// Закрытие попапа Image
+
+const closePopupImage = function() {
+  popupImage.classList.remove('popup_opened')
+  const fiqure = document.querySelector('.popup__image-fiqure')
+  fiqure.innerHTML = ''
+}
+
 // Закрытие попапа по клику на полупрозрачный фон
 const closePopupOutside = function(evt) {
   if (evt.target !== evt.currentTarget) { return } 
   closePopupInfo(evt)
   closePopupPlace(evt)
+  closePopupImage(evt)
 }
 
 // Изменение данных профиля
@@ -98,19 +119,20 @@ const showCards = function(place) {
   const placeElement = placeTemplate.cloneNode(true)
   const placeList = document.querySelector('.places__list')
 
-  // наполняем сожержимым из массива initialCards
+  // наполняем сожержимым из массива
   placeElement.querySelector('.place__image').src = place.link
-  placeElement.querySelector('.place__image').alt = place.alt
+  placeElement.querySelector('.place__image').alt = place.name
   placeElement.querySelector('.place__name').textContent = place.name
 
-  addNewPlaceListener(placeElement)
+  placeListener(placeElement)
   // отображаем на странице
   placeList.prepend(placeElement)
 }
 
-function addNewPlaceListener (placeElement) {
+const placeListener = function (placeElement) {
   placeElement.querySelector('.place__button-delete').addEventListener('click', deleteCard)
-  placeElement.querySelector('.place__button').addEventListener('click', like)
+  placeElement.querySelector('.place__button-like').addEventListener('click', like)
+  placeElement.querySelector('.place__image').addEventListener('click', openPopupImage)
 }
 
 // ДОБАВИМ НОВУЮ КАРТОЧКУ
@@ -141,7 +163,7 @@ const placeSubmitHandler = function(evt) {
 // ЛАЙКИ
 
 const like = function (evt) {
-  evt.target.classList.toggle('place__button_liked')
+  evt.target.classList.toggle('place__button-like_active')
 }
 
 // Удаление карточки
@@ -160,8 +182,10 @@ closeButtonInfo.addEventListener('click', closePopupInfo)
 closeButtonPlace.addEventListener('click', closePopupPlace)
 popupInfo.addEventListener('click', closePopupOutside)
 popupPlace.addEventListener('click', closePopupOutside)
+popupImage.addEventListener('click', closePopupOutside)
 formInfo.addEventListener('submit', formSubmitHandler)
 formPlace.addEventListener('submit', placeSubmitHandler)
+closeButtonImage.addEventListener('click', closePopupImage)
 
 // Перебор массива. Для каждого элемента применить функцию showCards
 initialCards.forEach(place => {
