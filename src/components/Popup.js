6e515889807b9318e26039f5b10b5c7c1df3cf69
Olya Salnikova <rilfayne@yandex.com>
2020-08-19 10:46:30 +0200

    hotfix: убрать листенер клика по esc при закрытом попапе

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,17 +1,18 @@
 export default class Popup {
   constructor(popup) {
     this._popup = popup;
+    this._handleEscClose = this._handleEscClose.bind(this)
   }
 
   open() {
     this._popup.classList.add('popup_opened')
 
-    document.addEventListener('keyup', (evt) => this._handleEscClose(evt))
+    document.addEventListener('keyup', this._handleEscClose)
   }
 
   close() {
     this._popup.classList.remove('popup_opened')
-    document.removeEventListener('keyup', (evt) => this._handleEscClose(evt))
+    document.removeEventListener('keyup', this._handleEscClose)
   }
 
   // Закрытие попапа по клику на Esc
